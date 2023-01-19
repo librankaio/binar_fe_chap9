5bf77af3458b844df9bf53266c98861a7c555017
Jawoto Tri Prabowo <jawototriprabowo@gmail.com>
2023-01-19 19:50:13 +0700

    [jawoto][FEAT][hompage-2]

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,55 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
-// import { styled } from '@mui/system';
-import { styled } from '@mui/material/styles';
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, Container, Grid, IconButton, Typography } from "@mui/material";
-import Paper from '@mui/material/Paper';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { red } from '@mui/material/colors';
+import "./styleHome.css";
 
-
-const Background = styled('div')({
-    minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/the-games-bg.jpg'})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-});
-const Title = styled('h1')({
-    padding: "0px",
-    margin: "0px",
-    fontFamily: 'monospace',
-    color: 'white',
-    fontSize: 50,
-});
-const MediumTitle = styled('h3')({
-    padding: "0px",
-    margin: "0px",
-    fontFamily: 'monospace',
-    color: 'white',
-});
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
-
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
 
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -64,16 +16,32 @@ import farcryImage from "../assets/farcry.jpeg";
 import nostalgic2Image from "../assets/nostalgic2.jpeg";
 import suicideImage from "../assets/suicide.jpeg";
 import wiiuImage from "../assets/wiiu.gif";
+import marioImage from "../assets/mario.jpeg"
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <div style={{ textAlign: "center" }}>
-        <h1>Main Game Impian</h1>
-        <h1>Sampai Pingsan</h1>
-        <br />
+      <div>
+          <Grid container columns={12} alignItems="center"
+              justifyContent="center" >
+              <img src={marioImage} alt="mario" />
+             </Grid>
+      </div>
+      <div style={{ textAlign: "center" }} className="homeText">
+        <div>Main Game Impian</div>
+        </div>
         <div>
+            <Grid container columns={12} alignItems="center"
+              justifyContent="center">
+                <Grid container className="trendingGames"
+              item
+              xs={4}
+              >
+                <div >Trending Games</div>
+                </Grid>
+                
+            </Grid>
           <Grid container columns={12}>
             <Grid
               container
@@ -294,7 +262,7 @@ function Home() {
           </Grid>
           <br />
         </div>
-      </div>
+      
     </div>
   );
 }
